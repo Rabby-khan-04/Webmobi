b9136dff3f0252ab8e16ce81f0e05f127feb9e8e
Rabby Khan <ajrabbyK72@gmail.com>
2023-06-20 23:09:35 +0600

    WebMobi Assignment Starts

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import MainLayout from "./Layout/MainLayout.jsx";
+import Home from "./Pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
