3e81d930a491083bd2ab18feb93de85e3602d395
Rabby Khan <ajrabbyK72@gmail.com>
2023-06-21 10:35:51 +0600

    Success Stories Section Added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Banner from "../../components/Home/Banner/Banner";
 import Experties from "../../components/Home/Experties/Experties";
+import SuccessStories from "../../components/Home/SuccessStories/SuccessStories";
 
 const Home = () => {
   return (
@@ -9,6 +10,8 @@ const Home = () => {
       <Banner />
       {/* Experties */}
       <Experties />
+      {/* Success Stories */}
+      <SuccessStories />
     </>
   );
 };
---
+++ src/components/Home/SuccessStories/SuccessStories.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./SuccessStories.css";
+
+const SuccessStories = () => {
+  return (
+    <section className="pt-32 pb-52 stories-sections">
+      <div className="container">
+        <div className="text-center text-white max-w-5xl mx-auto">
+          <h2 className="text-6xl mb-12">Success Stories</h2>
+          <p className="text-3xl">
+            Over the years, we have helped numerous startups successfully
+            establish their captive development centers. Join the roster of our
+            successful collaborations.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SuccessStories;
