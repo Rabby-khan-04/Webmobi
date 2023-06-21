286d026594da135e063ade3d8ef138393d612f0c
Rabby Khan <ajrabbyK72@gmail.com>
2023-06-21 10:42:50 +0600

    Added CAT

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "../../components/Home/Banner/Banner";
 import Experties from "../../components/Home/Experties/Experties";
 import SuccessStories from "../../components/Home/SuccessStories/SuccessStories";
+import CAT from "../../components/Home/CAT/CAT";
 
 const Home = () => {
   return (
@@ -12,6 +13,8 @@ const Home = () => {
       <Experties />
       {/* Success Stories */}
       <SuccessStories />
+      {/* Call To Action */}
+      <CAT />
     </>
   );
 };
---
+++ src/components/Home/CAT/CAT.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const CAT = () => {
+  return (
+    <section className="pt-16 pb-10 bg-dabyBlue">
+      <div className="container text-center">
+        <h2 className="text-dark text-6xl font-bold mb-4">
+          Ready to Scale Up?
+        </h2>
+        <p className="text-[#415661] text-2xl mb-7">
+          Experience the Incubyte advantage today. Let us help you navigate the
+          challenges and scale up your operations smoothly and efficiently.
+        </p>
+        <button className="bg-darkBlue py-7 px-12 text-2xl font-medium text-white rounded-xl">
+          Contact Us
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default CAT;
